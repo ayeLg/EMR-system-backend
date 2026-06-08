a26d4b8617ca66b03fc5cbca119634bde6ae6cb5
Moe-Wai-Yan <moorewaiyan@gmail.com>
2026-06-08 19:17:17 +0630

    fix: updateAppointmentDto

--- src/modules/appointments/dto/update-appointment.dto.ts
+++ src/modules/appointments/dto/update-appointment.dto.ts
@@ -1,7 +1,20 @@
 import { createZodDto } from 'nestjs-zod';
+import { z } from 'zod';
 import { CreateAppointmentSchema } from './create-appointment.dto';
 
-export const UpdateAppointmentSchema = CreateAppointmentSchema.partial();
+export const UpdateAppointmentSchema = CreateAppointmentSchema.extend({
+  status: z
+    .enum([
+      'SCHEDULED',
+      'ARRIVED',
+      'IN_PROGRESS',
+      'COMPLETED',
+      'CANCELLED',
+      'NO_SHOW',
+    ])
+    .optional(),
+  cancelledReason: z.string().optional(),
+}).partial();
 
 export class UpdateAppointmentDto extends createZodDto(
   UpdateAppointmentSchema,
